1d780b39ee1c26ed61d91335342fab647b73666e
Salman Farish <s91670002@gmail.com>
2026-09-20 22:53:57 +0530

    Fix search BasicTextField import

--- app/src/main/java/com/example/inchat/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/inchat/ui/search/SearchScreen.kt
@@ -2,7 +2,6 @@ package com.example.inchat.ui.search
 
 import android.content.Context
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
@@ -195,7 +195,7 @@ fun SearchScreen(
                             modifier = Modifier.width(9.dp)
                         )
 
-                        androidx.compose.foundation.BasicTextField(
+                        BasicTextField(
                             value = usernameInput,
                             onValueChange = { newValue ->
                                 usernameInput = newValue
