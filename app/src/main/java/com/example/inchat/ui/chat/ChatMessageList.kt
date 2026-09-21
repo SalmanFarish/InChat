72bdd4f8d333cd2ef37f28fc3395af77a216c4dd
Salman Farish <s91670002@gmail.com>
2026-09-21 19:34:33 +0530

    Add pointer consume import for chat gestures

--- app/src/main/java/com/example/inchat/ui/chat/ChatMessageList.kt
+++ app/src/main/java/com/example/inchat/ui/chat/ChatMessageList.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
