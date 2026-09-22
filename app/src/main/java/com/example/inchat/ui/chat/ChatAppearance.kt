ad7fd361d2b721f076c5e4cdb1608da0c5416b05
Salman Farish <s91670002@gmail.com>
2026-09-22 19:56:25 +0530

    Fix ChatAppearance Compose imports

--- app/src/main/java/com/example/inchat/ui/chat/ChatAppearance.kt
+++ app/src/main/java/com/example/inchat/ui/chat/ChatAppearance.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -30,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
