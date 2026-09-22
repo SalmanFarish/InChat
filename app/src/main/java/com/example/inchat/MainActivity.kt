ea7e74702af7c07dd090f77e8211a678aaacae93
Salman Farish <s91670002@gmail.com>
2026-09-22 14:55:35 +0530

    Fix Compose notification permission state delegates

--- app/src/main/java/com/example/inchat/MainActivity.kt
+++ app/src/main/java/com/example/inchat/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
