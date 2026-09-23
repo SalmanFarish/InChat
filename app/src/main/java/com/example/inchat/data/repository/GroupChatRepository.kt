2dfc8e2a916c568f9cdb9b481580edb82ab2fbee
Salman Farish <s91670002@gmail.com>
2026-09-23 23:24:27 +0530

    Fix group repository DatabaseError import

--- app/src/main/java/com/example/inchat/data/repository/GroupChatRepository.kt
+++ app/src/main/java/com/example/inchat/data/repository/GroupChatRepository.kt
@@ -6,6 +6,7 @@ import com.example.inchat.data.model.Message
 import com.example.inchat.data.model.ReplyTo
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ServerValue
 import kotlinx.coroutines.channels.awaitClose
